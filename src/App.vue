1b7b16606d521bd66d567375d6aaedbfe8533f65
JackPan <826647196@qq.com>
2023-03-07 20:18:36 +0800

    Add my first compoent

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <MyComponent/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MyComponent
+}
 }
 </script>
 
